17d2dc5f93284aabb608e8c4128f15c270f3e1e0
Tobias Froehlich <tfroehlich@Tobiass-MBP.fritz.box>
2021-05-17 16:11:44 +0200

    Hello World on localhost.

---
+++ src/main/java/com/neozo/ecommerce/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.neozo.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+
+}
