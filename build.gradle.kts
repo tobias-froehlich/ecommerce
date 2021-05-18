2f897cf61862a364c7269c66e1f0e5316d5c1e71
Tobias Froehlich <tfroehlich@Tobiass-MBP.fritz.box>
2021-05-18 08:23:49 +0200

    Gradle Kotlin. Tests work except that assertEquals... are not imported.

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("org.springframework.boot") version "2.4.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    java
+}
+
+group = "com.neozo"
+version = "0.0.1-SNAPSHOT"
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-freemarker")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    //implementation("org.flywaydb:flyway-core")
+    //runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+tasks {
+    "test"(Test::class) {
+        useJUnitPlatform()
+    }
+}
--- src/main/java/com/neozo/ecommerce/controller/Controller.java
+++ src/main/java/com/neozo/ecommerce/controller/Controller.java
@@ -3,6 +3,7 @@ package com.neozo.ecommerce.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class Controller {
 
